d262e43e48abc9a4f2a9bf226797fa6ab7e38e05
Da Evil <cryingpushpa.com>
2023-12-17 07:40:58 +0530

    modified:   api 	new file:   public/recommend/script.js 	new file:   public/recommend/style.css 	modified:   routes/home.js 	modified:   routes/user.js 	modified:   server.js 	new file:   views/home/home.ejs 	new file:   views/home/recommend.ejs

--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,18 @@
 const express = require('express')
 const router = express.Router()
 
-router.get("/:movi", (req,res) =>{
-  res.render("moviePage/moviePage")
+router.get("/", (req,res) =>{
+  let recoList = require("../routes/user.js").recoList ||  [8960, 2048,  1924,
+    271110, 1927, 76170,
+      1930,  268, 44943,
+     99861]
+      console.log(recoList);
+  res.render("home/home",{reco:recoList})
 
+        
+    })
+    
+router.get("/explore",(req,res)=>{
+    res.render("home/recommend")
 })
 module.exports = router
